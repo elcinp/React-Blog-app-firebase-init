1e421d6cb1c98268aa48debc5710bbdb09c865d0
elcinp <elcinpars1@gmail.com>
2021-10-02 11:28:14 +0300

    route added

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -3,6 +3,10 @@ import { BrowserRouter as Router,Switch,Route } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import Login from '../pages/Login'
 import Register from '../pages/Register'
+import Profile from '../pages/Profile'
+import Detail from '../pages/Detail'
+import NewBlog from '../pages/NewBlog'
+import UpdateBlog from '../pages/UpdateBlog'
 
 const AppRouter = () => {
     return (
@@ -11,6 +15,12 @@ const AppRouter = () => {
             <Switch>
                 <Route path="/login" component={Login}/>
                 <Route path="/register" component={Register}/>
+                <Route path="/ " exact component={Dasboard}/>
+                
+                <Route path="/profile " component={Profile}/>
+                <Route path="/new-blog " component={NewBlog}/>
+                <Route path="/update-blog " component={UpdateBlog}/>
+                <Route path="/detail " component={Detail}/>
             </Switch>
         </Router>
     )
